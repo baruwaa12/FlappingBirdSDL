3dfae030c55c38c007f65c6b13c4b9f26057286d
Shangoman <abdulbaruwa@hotmail.com>
2023-03-20 20:26:36 +0000

    Initial commit

---
+++ bird.cpp
@@ -0,0 +1,9 @@
+#include "bird.hpp"
+#include <SDL.h>
+#include <iostream>
+
+Bird::Bird(){}
+
+void Bird::fly(){
+  std::cout<<"Flying!" << std::endl;
+}
---
+++ bird.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <SDL.h>
+
+class Bird{
+  Bird();
+  public:
+    void fly();
+};
